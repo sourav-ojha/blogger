b0d9eabc618f2479dda0e9fec177c9c76f7da382
Bipul Jaiswal <devil@r3curs1v3-pr0xy.localdomain>
2022-09-03 23:41:44 +0530

    Like Post Added

--- server/models/Likes.js
+++ server/models/Likes.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const likes = mongoose.Schema({
     username: { type: String },
-    post_id: Number,
+    post_id: String,
 });
 
 module.exports = mongoose.model("likes", likes);
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,7 @@ const jwt = require("jsonwebtoken");
 const Post = require("../models/Posts");
 const crypto = require("crypto");
 const User = require("../models/User");
+const Likes = require("../models/Likes");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -32,13 +33,13 @@ router.get("/blog", async (req, res) => {
         const posts = await Post.find({ is_published: true }).select({ _id: 0, __v: 0 });
         res.status(200).send(posts);
     } catch (err) {
-        return res.status(500).send({ msg: "Something went wrong!" });
+        return res.status(500).json({ msg: "Something went wrong!" });
     }
 });
 
 //Get a specific post
 //No authentication required to view public posts
-router.get("/:post_id/", async (req, res) => {
+router.get("/blog/:post_id/", async (req, res) => {
     let post_id = sanitize(req.params.post_id);
 
     if (post_id) {
@@ -67,7 +68,7 @@ router.post("/blog", auth, async (req, res) => {
         try {
             let url = title.replace(/\s+/g, "-"); // Replace spaces with hyphens to form slug
             let post_id = crypto.randomBytes(8).toString("hex"); // Assign Unique post id to each post
-            url = post_id.toString() + "/" + url;
+            url = "/blog/" + post_id.toString() + "/" + url;
             let time_to_read = Math.round(content.split(" ").length / 200); // Calculate time to read the post
 
             let user_data = await User.findOne({ username: decodeJWT(req.header("Authorization").split(" ")[1]) }); // Get user details from the database
@@ -96,7 +97,7 @@ router.post("/blog", auth, async (req, res) => {
             return res.status(400).json({ msg: "Something went wrong!" });
         }
     } else {
-        return res.status(400).send({ msg: "Please provide a title, content, category and keywords!" });
+        return res.status(400).json({ msg: "Please provide a title, content, category and keywords!" });
     }
 });
 
@@ -122,8 +123,60 @@ router.delete("/blog/:post_id", auth, async (req, res) => {
             return res.status(400).json({ msg: "Something went wrong!" });
         }
     } else {
-        return res.status(400).send({ msg: "Please provide a post id!" });
+        return res.status(400).json({ msg: "Please provide a post id!" });
     }
 });
 
+// Search post as per keywords in the post title and keywords
+// Display in descending order of likes
+// No authentication required to search in public posts
+
+router.get("/search", async (req, res) => {
+    let search_query = sanitize(req.query.query); // Sanitize the data to prevent injection attacks
+
+    if (search_query) {
+        try {
+            let posts = await Post.find({ $or: [{ title: RegExp(search_query, "i") }, { keywords: RegExp(search_query, "i") }], is_published: true }).select({ _id: 0, __v: 0 });
+            if (posts.length > 0) {
+                res.status(200).send(posts);
+            } else {
+                return res.status(200).json({ msg: "No result found for " + search_query });
+            }
+        } catch (err) {
+            return res.status(500).json({ msg: "Something went wrong!" });
+        }
+    } else {
+        return res.status(400).json({ msg: "Please provide a search query!" });
+    }
+});
+
+// Like posts
+
+router.post("/blog/:post_id/like", auth, async (req, res) => {
+    let post_id = sanitize(req.params.post_id); // Sanitize the data to prevent injection attacks
+
+    if (post_id) {
+        try {
+            //Check if the user has already liked the post
+            let user_liked_post = await Likes.findOne({ post_id: post_id, username: decodeJWT(req.header("Authorization").split(" ")[1]) });
+            if (user_liked_post) {
+                //If post is already liked by the user, unlike the post
+
+                await Likes.findOneAndDelete({ post_id: post_id, username: decodeJWT(req.header("Authorization").split(" ")[1]) });
+                return res.status(200).json({ msg: "Post has beed disliked!" });
+            } else {
+                //Update likes in like collection
+                await Likes.create({
+                    post_id: post_id,
+                    username: decodeJWT(req.header("Authorization").split(" ")[1]),
+                });
+                return res.status(200).json({ msg: "Post liked successfully!" });
+            }
+        } catch (err) {
+            return res.status(400).json({ msg: "Something went wrong!" });
+        }
+    } else {
+        return res.status(400).json({ msg: "Please provide a post id!" });
+    }
+});
 module.exports = router;
